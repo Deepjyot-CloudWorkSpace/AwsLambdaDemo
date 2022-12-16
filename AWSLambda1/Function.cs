36012d7b666f2a492406c648ac6defbac6a0fa5c
Deepjyot Singh Kapoor <kapoordeepjyotsingh29@gmail.com>
2022-12-16 18:05:27 +0530

    first commit

---
+++ AWSLambda1/Function.cs
@@ -0,0 +1,30 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWSLambda1;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(User input, ILambdaContext context)
+    {
+        return input.Name.ToUpper();
+    }
+
+
+    public class User {
+
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+
+    }
+
+}
